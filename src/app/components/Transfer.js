8cedabd8cb41b847b48d12aa5cb2413d4d43f5d9
Antharithm <antharithm707@gmail.com>
2024-08-20 09:32:39 -0400

    tx details

--- src/app/components/Transfer.js
+++ src/app/components/Transfer.js
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 import { parseUnits } from "ethers";
 import styles from "./transfer.module.css";
 import { formatWeiAmount } from "../utils.js";
@@ -14,6 +15,9 @@ export default function Transfer({
 }) {
   const [txCostEth, setTxCostEth] = useState(undefined); // in Wei
   const [txCostUSD, setTxCostUSD] = useState(undefined); // in USD
+  const [sending, setSending] = useState(undefined); // send tx
+  const [txHash, setTxHash] = useState(undefined); // tx hash
+  const [error, setError] = useState(undefined); // tx error
 
   useEffect(() => {
     const init = async () => {
@@ -53,8 +57,37 @@ export default function Transfer({
     return `${formatWeiAmount(
       txCostUSD,
       nativeAsset.decimals
-    )} USD (${formatWeiAmount(txCostEth, nativeAsset.decimals)})`;
+    )} USD (${formatWeiAmount(txCostEth, nativeAsset.decimals)} ${
+      nativeAsset.ticker
+    })`;
   };
+
+  const getTransactionUrl = (hash) => {
+    return `${chain.blockchainExplorer}/${hash}`;
+  };
+
+  const send = async () => {
+    setSending(true);
+    try {
+      const txRequest = {
+        from: wallet.address,
+        to: transfer.to,
+        value: parseUnits(transfer.amount, transfer.asset.decimals),
+      };
+      const txResponse = await wallet.sendTransaction(txRequest);
+      const txReceipt = await txResponse.wait(); // wait for tx to be mined
+      console.log(txReceipt);
+      if (parseInt(txReceipt.status !== 1))
+        throw new Error("Transaction has failed!");
+      setTxHash(txReceipt.hash); // link to etherscan
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    } finally {
+      setSending(false);
+    }
+  };
+
   return (
     <div id={styles.overlay}>
       <div id={styles.transfer}>
@@ -113,7 +146,38 @@ export default function Transfer({
             disabled={true}
           />
         </div>
+        {sending && (
+          <div className="alert alert-info mt-3 mb-3">
+            <span role="img" aria-label="Loading" className="me-2">
+              ⏳
+            </span>
+            Processing transaction...
+          </div>
+        )}
+        {txHash && (
+          <div className="alert alert-success mt-3 mb-3">
+            <span role="img" aria-label="Success">
+              ✅
+            </span>{" "}
+            Transaction successful!{" "}
+            <Link
+              href={getTransactionUrl(txHash)}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View transaction details
+            </Link>
+          </div>
+        )}
+        {error && (
+          <div className="alert alert-danger mt-3 mb-3">
+            <strong>Error:</strong> Transaction failed. Please try again.
+          </div>
+        )}
         <div className="text-right">
+          <button className="btn btn-primary me-3" onClick={send}>
+            Submit
+          </button>
           <button
             className="btn btn-secondary"
             onClick={() => setShowTransferModal(false)}
